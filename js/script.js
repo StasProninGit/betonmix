b32d5601b9e9deff1a6320e90292f148bc42ecde
Stas <staspronin@mail.ru>
2023-05-29 17:59:03 +0300

    29.05 work

--- js/script.js
+++ js/script.js
@@ -47,3 +47,17 @@ function init() {
 }
 
 ymaps.ready(init);
+
+
+
+var swiper = new Swiper(".mySwiper", {
+  slidesPerView: 3,
+  spaceBetween: 30,
+  freeMode: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+});
+
+
